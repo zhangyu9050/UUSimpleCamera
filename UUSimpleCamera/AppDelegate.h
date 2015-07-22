1d2a3444eaef2b805182071d90ec3d8324f8a76e
zhangyu9050 <zhangyu@zhangyus-MacBook-Pro.local>
2015-07-22 15:01:11 +0800

    create

---
+++ UUSimpleCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UUSimpleCamera
+//
+//  Created by zhangyu on 15/7/22.
+//  Copyright (c) 2015年 zhangyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UUSimpleCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UUSimpleCamera
+//
+//  Created by zhangyu on 15/7/22.
+//  Copyright (c) 2015年 zhangyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
